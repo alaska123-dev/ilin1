3c349e6d5ef7386042bfa01330581c039b4767d7
ilin1 <cronp11@mail.ru>
2026-03-26 11:36:07 +0000

    Add Dockerfile and unit tests

---
+++ ilin1.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ilin1.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
--- obj/Debug/net8.0/ilin1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ilin1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ilin1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853ded3070e3902bbff8377a77f7d304fe0c2608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f67fde32c853dca6d39a72137bc1c4d64e94f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ilin1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ilin1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
